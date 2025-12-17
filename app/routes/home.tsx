3d671b3477d46b1b790a5f03bac162c8c4b70432
Setayesh <102351512+sititiy@users.noreply.github.com>
2025-12-17 10:43:29 +0330

    create authentication pages

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth, isLoading, fs, kv } = usePuterStore();
+  // const { id } = useParams();
+  // const [imageUrl, setImageUrl] = useState("");
+  // const [resumeUrl, setResumeUrl] = useState("");
+  // const [feedback, setFeedback] = useState<Feedback | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !auth.isAuthenticated) navigate(`/auth?next`);
+  }, [isLoading]);
+
+  return (
+    <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
+      <Navbar />
+      <section className="main-section py-16">
+        <div className="page-heading">
+          <h1>Track your application & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
